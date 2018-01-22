7e1bee87def1b4b784ef6d98f704358fbca4b0e2
John Hakala <john_hakala@brown.edu>
2018-01-22 16:22:13 -0500

    add functionality to generate DD trees with varied photon SFs

--- makeAllDDs.py
+++ makeAllDDs.py
@@ -7,30 +7,31 @@ debug = False
 first = True
 
 #for variation in [("nom", 0), ("up", 1), ("down", -1)]:
-for variation in [("nom", 0)]:
-  baseDir = path.join(getcwd(), "organize_smallifications")
-  categories = ["backgrounds", "signals", "data"]
-  #categories = ["signals"]
-  catDirs = {}
-  for category in categories:
-    catDirs[category] = path.join(baseDir, category)
-  
-  pprint(catDirs)
-  
-  outDir = baseDir.replace("smallifications", "DDs_btag-%s" % variation[0])
-  if not path.exists(outDir):
-    makedirs(outDir)
-  print "catDirs", catDirs
-  for catDir in catDirs:
-    catOutDir = path.join(outDir, catDir)
-    inputFiles = glob("%s/%s/*.root" % (baseDir, catDir))
+for btagVariation in [("nom", 0)]:
+  for phSFvariation in [("nom", 0), ("up", 1), ("down", -1)]:
+    baseDir = path.join(getcwd(), "organize_smallifications")
+    #categories = ["backgrounds", "signals", "data"]
+    categories = ["signals"]
+    catDirs = {}
+    for category in categories:
+      catDirs[category] = path.join(baseDir, category)
     
-    if not path.exists(catOutDir):
-      makedirs(catOutDir)
-    for inputFile in inputFiles:
-      if first:
-        print "about to call the first processHg" 
-        processHg(inputFile, inputFile.replace("smallified", "ddTree").replace("smallifications", "DDs_btag-%s" % variation[0]), False, False, variation[1])
-        first = False
-      elif not debug:
-        processHg(inputFile, inputFile.replace("smallified", "ddTree").replace("smallifications", "DDs_btag-%s" % variation[0]), True, True, variation[1])
+    pprint(catDirs)
+    
+    outDir = baseDir.replace("smallifications", "DDs_btag-%s_phSF-%s" % (btagVariation[0], phSFvariation[0]))
+    if not path.exists(outDir):
+      makedirs(outDir)
+    print "catDirs", catDirs
+    for catDir in catDirs:
+      catOutDir = path.join(outDir, catDir)
+      inputFiles = glob("%s/%s/*.root" % (baseDir, catDir))
+      
+      if not path.exists(catOutDir):
+        makedirs(catOutDir)
+      for inputFile in inputFiles:
+        if first:
+          print "about to call the first processHg" 
+          processHg(inputFile, inputFile.replace("smallified", "ddTree").replace("smallifications", "DDs_btag-%s_phSF-%s" % (btagVariation[0], phSFvariation[0])), False, False, btagVariation[1], phSFvariation[1])
+          first = False
+        elif not debug:
+          processHg(inputFile, inputFile.replace("smallified", "ddTree").replace("smallifications", "DDs_btag-%s_phSF-%s" % (btagVariation[0], phSFvariation[0])), True, True, btagVariation[1], phSFvariation[1])
